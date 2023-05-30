ec73169dcc5cd6238a27251d619b6fcd8bcf44d2
Emmanuel <mendezemmanuel1999@gmail.com>
2023-05-30 13:31:24 -0600

    implementation using cookies

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ const cors = require("cors");
 //libreria para manejar la autenticación
 const jwt = require("jsonwebtoken");
 
+//libreria para manejar cookies
+const cookies = require("cookie-session");
+
 //Libreria para manejar archivos
 const fs = require("fs");
 
 //libreria para validar parametros de entrada
-const {body,param,query,validationResult} = require("express-validator");
+const {body,param,query,validationResult, cookie} = require("express-validator");
 
 //ocultar x-powered-by (cabecera)
 app.disable("x-powered-by");
@@ -34,6 +37,12 @@ app.use(express.json());
 //se configura el cors usando la libreria.
 app.use(cors({allowedHeaders:"API-KEY"}));
 
+app.use(cookies({
+    name:"session",
+    httpOnly:true,
+    secret:process.env.COOKIES_SECRET,
+    maxAge: 10 * 60 * 60 * 1000, 
+}))
 
 //Configuracion del cors de manera manual
 /*app.use((req,res,next)=>{
@@ -47,7 +56,7 @@ app.use(cors({allowedHeaders:"API-KEY"}));
 })*/
 
 app.get("/",(req,res)=>{
-     res.send("<h1>Hola desde el servidor</h1>");
+     res.send("<h1>Taller 2 Backend for beginners</h1>");
 })
 
 app.get("/home",(req,res)=>{
@@ -56,6 +65,11 @@ app.get("/home",(req,res)=>{
     res.render("home",{name:"Emmanuel"});
 
 })
+
+
+app.get("/login",(req,res)=>{
+     return res.render("login");
+});
 
 
 /*
@@ -81,7 +95,11 @@ app.post("/login",
 
     //generacion del token -> (payload,private_key) 
     const token = jwt.sign({email:"bris@gmail.com",id:2},process.env.PRIVATE_KEY);
-    return res.status(200).json({token});
+    
+    //add cookies to request
+    req.session.data = JSON.stringify({token});
+    
+    return res.status(200).json({message:"successfull login"});
 
 });
 
@@ -89,7 +107,7 @@ app.post("/login",
  * Este endpoint esta protegido , solo los usuarios autenticados pueden acceder.
  * se utiliza el middleware auth para validar la autenticación
  */
-app.post("/contacts",auth,(req,res)=>{
+app.get("/contacts",auth,(req,res)=>{
 
     //obtenemos el usuario autenticado.
     const user = req.user;
@@ -104,20 +122,60 @@ app.post("/contacts",auth,(req,res)=>{
 })
 
 
+app.post("/contacts",
+auth,
+body("name").notEmpty().withMessage("The field name is required"),
+body("age").isNumeric().withMessage("The field age must be a number"),
+(req,res)=>{
+
+    const result = validationResult(req);
+    if(!result.isEmpty())
+        return res.status(400).json({errors:result.errors});
+
+    const {name,age} = req.body;
+    const data = fs.readFileSync("./contacts.json");
+    const contacts = JSON.parse(data);
+     
+    //obtener el usuario autenticado
+    const user = req.user;
+
+    contacts[`${user.id}`].contacts.push({
+        name,
+        age
+    });
+
+    fs.writeFileSync("./contacts.json",JSON.stringify(contacts));
+
+    return res.status(200).json({message:"Contact Created"})
+
+})
+
+
+
 /**
  *  Middleware para checar la autenticacion de un usuario.
  */
 function auth(req,res,next){
      
-     //leer la cabecera APII-KEY de la petición
-     const token = req.header("API-KEY");
 
-     //verificar que el usuario haya mandado un token.
-     if(!token){
-         return res.status(400).json({message:"Authentication is required"});
+     //leer token desde la cookie
+     const data = req.session.data;
+
+     //leer la cabecera APII-KEY de la petición
+     //const token = req.header("API-KEY");
+
+     if(!data){
+        return res.status(400).json({message:"Authentication is required"});
      }
 
+     //verificar que el usuario haya mandado un token.
+     /*if(!token){
+         return res.status(400).json({message:"Authentication is required"});
+     }*/
+
      try{
+
+        const {token} = JSON.parse(data);
 
         //verificamos el token y extraemos el payload
         const payload = jwt.verify(token,process.env.PRIVATE_KEY);
